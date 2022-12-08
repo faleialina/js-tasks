4827093c91b81068554aa338b8c8fe9007d193e6
faleialina <august3@mail.ru>
2022-12-08 23:59:28 +0300

    js-tasks-15

--- number/11/app.js
+++ number/11/app.js
@@ -1,2 +1,11 @@
-
+//Напишите программу, в которой вычисляется сумма, разность и произведение двух целых чисел, введенных с клавиатуры.
+const a = +prompt();
+const b = +prompt();
+if (Number.isInteger(a) && Number.isInteger(b)) {
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+} else {
+    console.log('введите целые цисла');
+}
 
--- number/14/app.js
+++ number/14/app.js
@@ -1,2 +1,10 @@
+//Вы вводите значение с клавиатуры. Если значение четное, то вывести true, в противном случае false
+const a = +prompt();
+b = a % 2
+if (b === 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
 
 
