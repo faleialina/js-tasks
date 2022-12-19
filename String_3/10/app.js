d8f188ca7ec684276686d65dc82767a1523aa0da
faleialina <august3@mail.ru>
2022-12-19 21:34:37 +0300

    String_3-10

--- String_3/10/app.js
+++ String_3/10/app.js
@@ -3,16 +3,18 @@
 // 1234 -> 1 неч 2 чет 3 неч 4 чет
 // 7 -> 7 – неч
 // 444444 -> 4 чет 4 чет 4 чет 4 чет 4 чет 4 чет
-const a = 1234;
+const a = '1234';
 
 if (!isNaN(a)) {
+    let result = '';
     for (let i = 0; i < a.length; i++) {
         if (a[i] % 2 === 0) {
-            console.log('чет')
+            result += a[i]  + ` чет `;
         } else {
-            console.log('неч');
+            result += a[i]  + ` неч `;
         }
     }
+    console.log(result);
 } else {
     console.log('Введите число');
 }
