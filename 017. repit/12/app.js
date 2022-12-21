f34611d04044ab1036ba2ea824b7679d7ca80fb4
faleialina <august3@mail.ru>
2022-12-21 21:39:59 +0300

    017. repit_1-8

--- 017. repit/12/app.js
+++ 017. repit/12/app.js
@@ -2,8 +2,9 @@
 // '-1-2-3-4-5-6-7-8-9-'
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-result = ''
+let result = ''
 for (let i = 0; i < arr.length; i++) {
-    result += `-` + arr[i];
+    if (i === 0) result += '-'
+    result += arr[i] + `-`;
 }
-console.log(result + '-')
+console.log(result)
