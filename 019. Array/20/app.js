aa011591470d1256bb59cb7faaf6e5fba12ca0c7
faleialina <august3@mail.ru>
2022-12-26 22:03:27 +0300

    019.Array/20

--- 019. Array/20/app.js
+++ 019. Array/20/app.js
@@ -2,11 +2,15 @@
 // между двумя нечетными числами. Работать с числом как с массивом.
 // Использовать for
 // 55639217 -> 5:563:921:7.
-const arr = [1, 2, 3, 4, -1, 6, 7, 8, 9, 10];
-let value = arr[0];
+let a = '55639217';
+let arr = a.split('');
+
+let result = '';
 for (let i = 0; i < arr.length; i++) {
-    if (value > arr[i]) {
-        value = arr[i];
+    if (arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0) {
+        result += arr[i] + ':';
+    } else {
+        result += arr[i];
     }
 }
-console.log(value)
+console.log(result)
