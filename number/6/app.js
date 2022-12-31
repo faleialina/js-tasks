242b693cd1c711d5b318b06b7cff4fe39b4107e5
faleialina <august3@mail.ru>
2022-12-31 23:01:31 +0300

    Numder/dop

--- number/6/app.js
+++ number/6/app.js
@@ -1,7 +1,7 @@
 //Вы вводите 2 числа a и b. Вам необходимо решить пример (2(а-b)-4(b-a))/2
 
-const a = +prompt();
-const b = +prompt();
+const a = +prompt('введите число а');
+const b = +prompt('введите число в');
 const s =  (2 * (a - b)-4*(b-a))/2;
 console.log(s);
 
