175b867ba2a94d997d104949de25be9007750a1f
faleialina <august3@mail.ru>
2023-01-01 23:25:47 +0300

    Number/11

--- number/11/app.js
+++ number/11/app.js
@@ -1,11 +1,11 @@
 //Напишите программу, в которой вычисляется сумма, разность и произведение двух целых чисел, введенных с клавиатуры.
-const a = +prompt();
-const b = +prompt();
+const a = +prompt('введите целые цисла');
+const b = +prompt('введите целые цисла');
 if (Number.isInteger(a) && Number.isInteger(b)) {
     console.log(a + b);
     console.log(a - b);
     console.log(a * b);
 } else {
-    console.log('введите целые цисла');
+    console.log('ошибка ввода');
 }
 
