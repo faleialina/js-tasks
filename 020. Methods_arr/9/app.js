8d2cf5f407fce14171d24428981240a695b7e835
faleialina <august3@mail.ru>
2023-01-05 23:11:56 +0300

    020. Methods_arr/9-10

--- 020. Methods_arr/9/app.js
+++ 020. Methods_arr/9/app.js
@@ -1,9 +1,26 @@
-// Дан массив 2, 5, 9, 15, 0, 4. Выведите все числа кратные 3 циклом for of
-
-const arr = [2, 5, 9, 15, 0, 4];
-for (const i of arr) {
-    if (i % 3 == 0) {
-        console.log(i);
-    }
+// На входе n – количество элементов массива. Далее производится заполнение
+// массива с клавиатуры. Выведите сумму всех элементов массива. Проверки на
+// ввод только чисел. Использовать forEach, reduce
+const arr = [];
+const n = prompt('количество элементов массива');
+for (let i = 0; i < n; i++) {
+   arr.push(prompt('заполнение массива'))
 }
+// //1-й способ
+arr.forEach(function (elem) {
+let sum = 0
+   if (!isNaN(elem)) {
+    sum+=elem
+      console.log(sum);
+   } else {
+      console.log('ввод только чисел');
+   }
+});
 
+
+
+// let result = arr.reduce(function(sum, elem) {
+// 	return sum + elem;
+// }, 0);
+
+// console.log(result);
