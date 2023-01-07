7a95879a89d712333d3a3a2c3f0bab37c9fe8566
faleialina <august3@mail.ru>
2023-01-07 18:32:17 +0300

    020. Methods_arr/7plus

--- 020. Methods_arr/7/app.js
+++ 020. Methods_arr/7/app.js
@@ -2,14 +2,53 @@
 // способы обращения к первому и последнему элементам
 
 
-const arr =['hschool', 'hschool_0', 'hschool_1'];
-//1-й способ
-console.log(arr[0]);
-console.log(arr[2]);
+const arr = ['hschool', 'hschool_0', 'hschool_1'];
+// //1-й способ
+// console.log(arr[0]);
 
-//2-й способ
-console.log(arr[0]);
-arr.reverse()
-console.log(arr[0]);
+// console.log(arr[2]);
 
+// //2-й способ
+// console.log(arr[0]);
 
+// arr.reverse()
+// console.log(arr[0]);
+
+// //3-й способ
+// for (let i = 0; i < arr.length; i++) {
+//     if (i === 0) {
+//         console.log(arr[i]);
+//     };
+
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (i === arr.length-1) {
+//         console.log(arr[i]);
+//     };
+
+// }
+
+// //4-й способ
+// console.log(arr.shift());
+// console.log(arr.pop());
+
+// //5-й способ
+// arr.forEach(function (el, index) {
+//     if (index === 0) {
+//         console.log(el);
+//     }
+// })
+
+// arr.forEach(function (el, index) {
+//     if (index === arr.length - 1) {
+//         console.log(el);
+//     }
+// })
+// //6-й способ
+// console.log(arr[arr.length - arr.length]);
+// console.log(arr[arr.length - 1]);
+
+//7-й способ
+console.log(arr.slice(0,1));
+console.log(arr.slice(2,3));
--- 020. Methods_arr/9/app.js
+++ 020. Methods_arr/9/app.js
@@ -7,20 +7,20 @@ for (let i = 0; i < n; i++) {
    arr.push(prompt('заполнение массива'))
 }
 //1-й способ
-arr.forEach(function (elem) {
-let sum = 0
-   if (!isNaN(elem)) {
-    sum+=elem
-      console.log(sum);
-   } else {
-      console.log('ввод только чисел');
-   }
-});
+// let sum = 0
+// arr.forEach(function (elem) {
+//    if (!isNaN(elem)) {
+//     sum+=elem
+//       console.log(sum);
+//    } else {
+//       console.log('ввод только чисел');
+//    }
+// });
 
 
 //1-й способ
-// let result = arr.reduce(function(sum, elem) {
-// 	return sum + elem;
-// }, 0);
+let result = arr.reduce(function(sum, elem) {
+	return sum + elem;
+}, 0);
 
-// console.log(result);
+console.log(result);
